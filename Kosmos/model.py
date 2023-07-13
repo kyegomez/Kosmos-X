12d39e55718e708fb40df8ee5694441ce1e2b908
Kye <kye@apacmediasolutions.com>
2023-07-12 20:21:12 -0400

    clean up + error handling for Kosmos

--- Kosmos/model.py
+++ Kosmos/model.py
@@ -2,6 +2,7 @@
 # Remaining imports
 import logging
 import torch
+import torch.nn as nn
 from torch.nn import Embedding, Module
 from transformers import T5Tokenizer, CLIPProcessor, CLIPModel, PreTrainedTokenizerFast, AutoTokenizer
 from tokenizers import SentencePieceBPETokenizer
@@ -74,30 +75,22 @@ class KosmosTokenizer:
             logging.error(f"Failed to tokenize sample: {e}")
             raise
 
-
-class Kosmos(Module):
+class Kosmos(nn.Module):
     def __init__(self):
         super().__init__()
+
         # Instantiate Clip Vit-l/14
-        self.clip_model = CLIPModel.from_pretrained("laion/CLIP-ViT-L-14-laion2B-s32B-b82K").vision_model
+        try:
+            self.clip_model = CLIPModel.from_pretrained("laion/CLIP-ViT-L-14-laion2B-s32B-b82K").vision_model
+        except Exception as e:
+            logging.error(f"Failed to initialize CLIP model: {e}")
+            raise
 
-        self.embed = bitsandbytes.nn.modules.Embedding(
-            32002,
-            2048,
-            padding_idx=1
-        )
-        self.embed_positions= PositionalEmbedding(
-            2048,
-            2048,
-            1
-        )
+        self.embed = bitsandbytes.nn.modules.Embedding(32002, 2048, padding_idx=1)
+        self.embed_positions= PositionalEmbedding(2048, 2048, 1)
 
-        self.output_projection = torch.nn.Linear(
-            2048, 32002, bias=False
-        )
-        torch.nn.init.normal_(
-            self.output_projection.weight, mean=0, std=2048**-0.5
-        )
+        self.output_projection = nn.Linear(2048, 32002, bias=False)
+        nn.init.normal_(self.output_projection.weight, mean=0, std=2048**-0.5)
 
         # Config following KOSMOS-1 paper (https://arxiv.org/pdf/2302.14045.pdf)
         self.config = DecoderConfig(
@@ -114,18 +107,19 @@ class Kosmos(Module):
             multiway=True,
             max_rel_pos=2048,
             alibi_pos_bias=True,
-            # flash_attention=True
-            # qk_norm=True
-            #one_quert_write_head=True
             alibi_num_heads=16,
-
         )
-        self.decoder = Decoder(
-            self.config,
-            embed_tokens=self.embed,
-            embed_positions=self.embed_positions,
-            output_projection=self.output_projection
-        )
+        
+        try:
+            self.decoder = Decoder(
+                self.config,
+                embed_tokens=self.embed,
+                embed_positions=self.embed_positions,
+                output_projection=self.output_projection
+            )
+        except Exception as e:
+            logging.error(f"Failed to initialize Decoder: {e}")
+            raise
 
         self.perceive = PerceiverResampler(
             dim = 1024,
@@ -136,18 +130,31 @@ class Kosmos(Module):
             num_media_embeds = 257
         )
 
-        self.image_proj = torch.nn.Linear(1024, 2048, bias=False)
-        torch.nn.init.normal_(
-            self.image_proj.weight, mean=0, std=2048**-0.5
-        )
+        self.image_proj = nn.Linear(1024, 2048, bias=False)
+        nn.init.normal_(self.image_proj.weight, mean=0, std=2048**-0.5)
 
-    def forward(self, text_tokens, images, **kwargs):
-        images = self.clip_model(pixel_values=images)["last_hidden_state"]
-        images = self.perceive(images).squeeze(1)
-        images = self.image_proj(images)
+    def forward(self, text_tokens: torch.Tensor, images: torch.Tensor, **kwargs):
+        if not isinstance(text_tokens, torch.Tensor) or not isinstance(images, torch.Tensor):
+            raise TypeError("text_tokens and images must be instances of torch.Tensor")
 
-        model_input = self.decoder.forward_embedding(text_tokens)[1]
-        model_input = torch.cat([model_input[:, 0:2], images, model_input[:, 2:]], dim=1)
-        model_input = self.decoder.forward_embedding(model_input, token_embedding=model_input)[0]
+        try:
+            images = self.clip_model(pixel_values=images)["last_hidden_state"]
+            images = self.perceive(images).squeeze(1)
+            images = self.image_proj(images)
+        except Exception as e:
+            logging.error(f"Failed during image processing: {e}")
+            raise
 
-        return self.decoder(model_input, passed_x=model_input)[0]
+        try:
+            model_input = self.decoder.forward_embedding(text_tokens)[1]
+            model_input = torch.cat([model_input[:, 0:2], images, model_input[:, 2:]], dim=1)
+            model_input = self.decoder.forward_embedding(model_input, token_embedding=model_input)[0]
+        except Exception as e:
+            logging.error(f"Failed during text processing: {e}")
+            raise
+
+        try:
+            return self.decoder(model_input, passed_x=model_input)[0]
+        except Exception as e:
+            logging.error(f"Failed during model forward pass: {e}")
+            raise
